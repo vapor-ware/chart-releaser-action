58bc0655bd957b3f5275d4acc4477de09a662aad
Sam Foo <sam.foo@vapor.io>
2022-08-12 11:20:19 -0700

    Add working directory to git safe

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,8 @@
 set -e
 
 cd "$GITHUB_WORKSPACE" || exit
+
+sh -c "git config --global --add safe.directory $GITHUB_WORKSPACE"
 sh -c "chart-releaser $1"
 
 echo "::set-output name=chart-releaser::chart-releaser step completed"
